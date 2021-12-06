c63c8823b2e2d17391bd7e0ba48d02605926cc1a
Łukas Gawry <lukaszgawrys13@gmail.com>
2021-12-06 15:51:56 +0100

    create lists from arrays and finish

--- index.js
+++ index.js
@@ -1,48 +1,66 @@
 const btn = document.querySelector('.btn');
+const evenList = document.querySelector('.main__columns__left__evenList');
+const oddList = document.querySelector('.main__columns__right__oddList');
 
 let arrTwenty = [];
 let arrOdd = [];
 let arrEven = [];
 
-function getRandomNums(min, max) {
+//random numbers from 1 to 100
+function getRandomNumbers(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function numsToArr() {
+//20 random numbers to array
+function numbersToArr() {
     for (let i=0; i<20; i++){
-    let myNums = getRandomNums(1,100);
+    let myNums = getRandomNumbers(1,100);
     console.log(myNums);
     arrTwenty.push(myNums);
 }}
 
+//even numbers from array of 20 to new array
 function evenNumbers(num) {
     if (num % 2 === 0) {
         arrEven.push(num);
     }
 }
+
+
+//odd numbers from array of 20 to new array
 function oddNumbers(num) {
     if (num % 2 === 1) {
         arrOdd.push(num);
     }
 }
 
-function oddNumbers(num) {
-    if (num % 2 === 1) {
-        arrOdd.push(num);
-    }
+//create li elems with even numbers
+function evenNumbersToList(num){
+    let li = document.createElement('li');
+    li.textContent = num;
+    evenList.appendChild(li);
 }
 
+//create li elems with odd numbers
+function oddNumbersToList(num){
+    let li = document.createElement('li');
+    li.textContent = num;
+    oddList.appendChild(li);
+}
+
+//btn onClick event
 btn.addEventListener('click', function (e){
     e.preventDefault();
     arrTwenty = [];
     arrEven = [];
     arrOdd = [];
-    numsToArr();
-    console.log(arrTwenty);
+    evenList.innerHTML = null;
+    oddList.innerHTML = null;
+    numbersToArr();
     arrTwenty.forEach(evenNumbers);
     arrEven = arrEven.sort(function (a, b) {  return a - b;  });
-    console.log(arrEven);
+    arrEven.map(evenNumbersToList);
     arrTwenty.forEach(oddNumbers);
     arrOdd = arrOdd.sort(function (a, b) {  return a - b;  });
-    console.log(arrOdd);
+    arrOdd.map(oddNumbersToList);
 })

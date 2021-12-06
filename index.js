ba7439e02edc5dc94c8cc2f2a7efd212cea05caf
Łukas Gawry <lukaszgawrys13@gmail.com>
2021-12-06 14:15:15 +0100

    get random nums

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-alert('hello');
+let arrTwenty = [];
+const btn = document.querySelector('.btn');
+
+function getRandomNums(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function numsToArr() {
+    for (let i=0; i<20; i++){
+    let myNums = getRandomNums(1,100);
+    console.log(myNums);
+    arrTwenty.push(myNums);
+}}
+
+btn.addEventListener('click', function (e){
+    e.preventDefault();
+    arrTwenty = [];
+    numsToArr();
+    console.log(arrTwenty);
+})
